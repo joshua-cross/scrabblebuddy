eca708d7105622e44be01c8448645fea63402dc7
joshua-cross <crossjoshua6@gmail.com>
2020-01-16 13:31:05 +0000

    Sorts the anagrams based on score.

--- src/components/ScrabbleForm/AnagramForm.vue
+++ src/components/ScrabbleForm/AnagramForm.vue
@@ -6,7 +6,8 @@
         <p>{{ submitWord }}</p>
         <button @click="submit">Submit</button>
         <div v-for="(anagram, index) in anagrams" :key="index">
-            {{anagram}}
+            {{anagram.word}}
+            {{anagram.score}}
         </div>
     </div>
 </template>
@@ -14,6 +15,7 @@
 <script>
 import ScrabbleTiles from './ScrabbleTiles';
 import axios from 'axios';
+import {cost} from '../../utils/scrabbleCost';
 export default {
     data() {
         return {
@@ -39,8 +41,23 @@ export default {
                                   .replace(/\s+/g, " ");
             var anagrams = await axios.get(`http://www.anagramica.com/all/:${word}`);
             //filtering all words above 1 character.
-            this.anagrams = anagrams.data.all.filter((word) => word.length > 1);
-            console.log("Anagrams: ", anagrams);
+            var words = anagrams.data.all.filter((word) => word.length > 1);
+            var wordsObject = [];
+            words.forEach(word => {
+                var totalScore = 0
+                for(let i = 0; i < word.length; i++) {
+                    totalScore += cost(word[i]);
+                }
+                wordsObject.push({
+                    word: word,
+                    score: totalScore
+                })
+            });
+
+            //sorting words based on the score they get in Scrabble e.g. Quizzed would come before quiz.
+            wordsObject.sort((a, b) => (b.score > a.score) ? 1 : -1);
+            this.anagrams = wordsObject;
+        
         },
         removeChars(word) {
             return word.length > 1;
